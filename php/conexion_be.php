d01eba5840f4256da293e2c785ae574b7c34c231
Lumar Code <marce.virgo20@gmail.com>
2024-06-21 00:51:32 -0500

    se ha modificado la conexion bd

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,17 +1,15 @@
 <?php
 
-   $servername = "localhost:3307"; // Nombre del servidor
-$username = "root";   // Nombre de usuario de la base de datos
-$password = ""; // Contraseña del usuario
-$dbname = "domosarquitectos"; // Nombre de la base de datos
+    $servername = "localhost:3307";
+    $username = "root"; 
+    $password = ""; 
+    $dbname = "domosarquitectos"; 
 
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-echo "Conexión exitosa";
+    if ($conn->connect_error) {
+        die("Conexión fallida: " . $conn->connect_error);
+    }
+    echo "Conexión exitosa";
     
 ?>
